33e49aa2f22b5963e2f9cf7f334a132b093fdbed
andfel28 <34451185+andfel28@users.noreply.github.com>
2018-02-23 15:21:24 -0500

    Add files via upload

---
+++ matrices_omp.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+void main ()
+{
+
+    int filas;
+    int columnas;
+
+    int i;
+    int c;
+    int k;
+
+    float **A;
+    float **B;
+    float **producto;
+    float suma;
+    char *cadenaM1;
+    char *cadenaM2;
+    char *cadenaProducto;
+    char *cadenaFile;
+
+    FILE *FP;
+
+    // Generamos una semilla para que el random genere siempre valores diferentes:
+    srand(time(NULL));
+
+    // Leemos el tamaño de los matrizes:
+    printf ( "Filas:" );
+    scanf ( "%d", &filas);
+
+    printf ( "Columnas:" );
+    scanf ( "%d", &columnas);
+
+
+    // Reservamos memoria para los matrizes:
+
+    A = (float **)malloc(filas*sizeof(float*));
+	for (i=0;i<filas;i++)
+		A[i] = (float*)malloc(columnas*sizeof(float));
+
+    B = (float **)malloc(columnas*sizeof(float*));
+	for (i=0;i<columnas;i++)
+		B[i] = (float*)malloc(filas*sizeof(float));
+
+    producto = (float **)malloc(filas*sizeof(float*));
+	for (i=0;i<filas;i++)
+		producto[i] = (float*)malloc(filas*sizeof(float));
+
+    //Cadenas de texto a almacenar e el file.csv
+    cadenaM1       = ( char * ) malloc ( 9000 * sizeof ( char ) );
+    cadenaM2       = ( char * ) malloc ( 9000 * sizeof ( char ) );
+    cadenaProducto = ( char * ) malloc ( 9000 * sizeof ( char ) );
+    cadenaFile     = ( char * ) malloc ( 9000 * sizeof ( char ) );
+
+    if ( A==NULL||B==NULL||producto==NULL||cadenaM1==NULL||cadenaM2==NULL||cadenaProducto==NULL||cadenaFile==NULL)
+    {      perror ( "Problemas reservando memoria" ); exit ( 1 ); }
+
+    strcpy( cadenaM1, "M1:\n" );
+    strcpy( cadenaM2, "M2:\n" );
+    strcpy( cadenaProducto, "Producto:\n" );
+
+    //Iniciliza la matriz A
+    for(i=0;i<filas;i++)
+        for(c=0;c<columnas;c++)
+              A[i][c]  = rand()%1000;
+
+    //Iniciliza la matriz B
+    for(i=0;i<columnas;i++)
+        for(c=0;c<filas;c++)
+              B[i][c]  = rand()%1000;
+
+   // Procedimiento para realizar el producto de matrices A x B
+
+       for(i=0;i<filas;i++)
+       {
+           #pragma omp parallel shared(A,B, filas,columnas, i) private(suma,c) num_threads(4)
+           {
+                   #pragma omp for schedule(dynamic,3)
+                   for(c=0;c<filas;c++)
+                   {
+                       suma = 0.0;
+                       for(k=0;k < columnas;k++)
+                            suma= A[i][k]*B[k][c] + suma;
+
+                       producto[i][c]=suma;
+                  }
+           }
+       }
+
+
+    //Zona de cargar matrices a texto
+    for(i=0;i<filas;i++)
+    { for(c=0;c<columnas;c++)
+      {
+           sprintf(cadenaFile,"%f",A[i][c]);
+           strcat( cadenaM1, cadenaFile );
+           strcat( cadenaM1, ", " );
+      }
+      strcat( cadenaM1, "\n" );
+    }
+
+    for(i=0;i<columnas;i++)
+    { for(c=0;c<filas;c++)
+      {
+           sprintf(cadenaFile,"%f",B[i][c]);
+           strcat( cadenaM2, cadenaFile );
+           strcat( cadenaM2, ", " );
+      }
+      strcat( cadenaM2, "\n" );
+    }
+
+    for(i=0;i<filas;i++)
+    { for(c=0;c<filas;c++)
+      {
+           sprintf(cadenaFile,"%f",producto[i][c]);
+           strcat( cadenaProducto, cadenaFile );
+           strcat( cadenaProducto, ", " );
+      }
+      strcat( cadenaProducto, "\n" );
+    }
+//----------------------------------------
+
+    strcpy( cadenaFile,"");
+    strcat( cadenaFile, cadenaM1 );
+    strcat( cadenaFile, "\n" );
+    strcat( cadenaFile, cadenaM2 );
+    strcat( cadenaFile, "\n" );
+    strcat( cadenaFile, cadenaProducto );
+
+    free ( A );
+    free ( B );
+    free ( producto );
+    free ( cadenaM1 );
+    free ( cadenaM2 );
+    free ( cadenaProducto );
+
+    //Almacenamos la informacion en un archivo
+    FP = fopen("d:/gcc/matrices.txt","w");
+    if (FP==NULL){printf("Ocurrio un error"); free ( cadenaFile ); exit (1);}
+    fputs(cadenaFile,FP);
+    fclose(FP);
+
+    free ( cadenaFile );
+
+}
---
+++ sumaVectores.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void main ()
+{
+
+    int numeroElementos;
+    int i;
+    float *vector;
+    float *vector2;
+    float *suma;
+
+    char *cadenaV1;
+    char *cadenaV2;
+    char *cadenaSuma;
+    char *cadenaFile;
+
+    FILE *FP;
+
+    // Generamos una semilla para que el random genere siempre valores diferentes:
+    srand(time(NULL));
+
+    // Leemos el tamaño de los vectores:
+    printf ( "¿Tamaño del vector? " );
+    scanf ( "%d", &numeroElementos );
+
+    // Reservamos memoria para los vectores:
+    vector  = ( float * ) malloc ( numeroElementos * sizeof ( float ) );
+    vector2 = ( float * ) malloc ( numeroElementos * sizeof ( float ) );
+    suma =    ( float * ) malloc ( numeroElementos * sizeof ( float ) );
+
+    //Cadenas de texto a almacenar e el file.csv
+    cadenaV1   = ( char * ) malloc ( 3000 * sizeof ( char ) );
+    cadenaV2   = ( char * ) malloc ( 3000 * sizeof ( char ) );
+    cadenaSuma = ( char * ) malloc ( 3000 * sizeof ( char ) );
+    cadenaFile = ( char * ) malloc ( 3000 * sizeof ( char ) );
+
+
+    if ( vector==NULL||vector2==NULL||suma==NULL||cadenaV1==NULL||cadenaV2==NULL||cadenaSuma==NULL||cadenaFile==NULL)
+    {      perror ( "Problemas reservando memoria" ); exit ( 1 ); }
+
+    cadenaV1   = strcpy( cadenaV1, "V1: " );
+    cadenaV2   = strcpy( cadenaV2, "V2: " );
+    cadenaSuma = strcpy( cadenaSuma, "Suma: " );
+
+    for(i=0;i<numeroElementos;i++)
+    {
+        vector[i]  = rand()%1000;
+        vector2[i] = rand()%1000;
+        suma[i]    = vector2[i] + vector[i];
+
+
+        sprintf(cadenaFile,"%f",vector[i]);
+        strcat( cadenaV1, cadenaFile );
+        strcat( cadenaV1, "," );
+
+        sprintf(cadenaFile,"%f",vector2[i]);
+        strcat( cadenaV2, cadenaFile );
+        strcat( cadenaV2, "," );
+
+        sprintf(cadenaFile,"%f",suma[i]);
+        strcat( cadenaSuma, cadenaFile );
+        strcat( cadenaSuma, "," );
+
+    }
+
+    strcpy( cadenaFile,"");
+    strcat( cadenaFile, cadenaV1 );
+    strcat( cadenaFile, "\n" );
+    strcat( cadenaFile, cadenaV2 );
+    strcat( cadenaFile, "\n" );
+    strcat( cadenaFile, cadenaSuma );
+
+    free ( vector );
+    free ( vector2 );
+    free ( suma );
+    free ( cadenaV1 );
+    free ( cadenaV2 );
+    free ( cadenaSuma );
+
+    FP = fopen("d:/gcc/vectores.txt","w");
+    if (FP==NULL){printf("Ocurrio un error"); free ( cadenaFile ); exit (1);}
+    fputs(cadenaFile,FP);
+    fclose(FP);
+
+    free ( cadenaFile );
+}
